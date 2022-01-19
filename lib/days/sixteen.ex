588e88ccb8538a32615e3a4f10bb049e60512f64
JP <jp.bonner@bonner.jp>
2022-01-19 18:17:37 -0500

    day 16: packet decoder pt 2

--- lib/days/sixteen.ex
+++ lib/days/sixteen.ex
@@ -2,38 +2,53 @@ defmodule AdventOfCode2021.Sixteen do
   @moduledoc """
   Day 16: Packet Decoder
   https://adventofcode.com/2021/day/16
+
+  Parse bytecode, build an AST, and execute it.
   """
 
   defmodule Packet do
+    @operators %{
+      0 => &Enum.sum/1,
+      1 => &Enum.product/1,
+      2 => &Enum.min/1,
+      3 => &Enum.max/1,
+      5 => &AdventOfCode2021.Sixteen.gt/2,
+      6 => &AdventOfCode2021.Sixteen.lt/2,
+      7 => &AdventOfCode2021.Sixteen.equality/2
+    }
     @doc """
-    Create a new packet, peeling version + type from header bits.
+    Create a new, single top-level packet from a hex string.
     """
-    # Need to specific body::bits because bitstring will default to bytes,
-    # and remaining bits in body probably aren't divisible by 4
     def new(input_string) do
-      input_string
+      packet = input_string
       |> Base.decode16!()
       |> decode()
+
+      case packet do
+        {_rem, packet} -> packet # only one top-level packet
+        packet -> packet
+      end
     end
 
     def decode(bitstring), do: decode(bitstring, [])
 
     # leftover garbage bits
     def decode("", packets), do: packets
-    def decode(<<_::3, _::0>>, packets), do: packets
+    def decode(<<_::1>>, packets), do: packets
+    def decode(<<_::2>>, packets), do: packets
+    def decode(<<_::3>>, packets), do: packets
 
     def decode(<<version::3, body::bits>>, packets) do
-      case decode_type(body, [%{version: version, bit_size: 3} | packets]) do
-        {rem, new_packets} -> decode(rem, new_packets)
-        new_packets -> new_packets
-      end
+      decode_type(body, [%{version: version, bit_size: 3} | packets])
     end
-
-    # leftover garbage bits
-    def decode(_, [_packet | packets]), do: packets
 
     ##
     # Types
+
+    # garbage bits
+    defp decode_type(<<_::1>>, [_packet | packets]), do: packets
+    defp decode_type(<<_::2>>, [_packet | packets]), do: packets
+    defp decode_type(<<_::3>>, [_packet | packets]), do: packets
 
     defp decode_type(<<4::3, body::bits>>, [packet | packets]) do
       packet =
@@ -44,45 +59,45 @@ defmodule AdventOfCode2021.Sixteen do
       decode_literal(body, [packet | packets])
     end
 
+    # garbage bits
+    defp decode_type(<<_::4>>, [_packet | packets]), do: packets
+
     # by length
     defp decode_type(
            <<type::3, 0::1, packets_size::15, payload::size(packets_size)-bits, rest::bits>>,
            [packet | packets]
          ) do
-      packet =
-        packet
-        |> Map.put(:type, type)
-        |> Map.put(:length_type_id, 0)
-        |> Map.put(:should_contain_bits, packets_size)
+      packet = handle_operator_data(packet, type, 0)
 
       subpackets = decode_operator(:length, packets_size, payload, [])
 
-      {rest, [process_subpackets(packet, subpackets) | packets]}
+      {rest, [handle_operator_subpackets_data(packet, subpackets) | packets]}
     end
 
     # by count
     defp decode_type(<<type::3, 1::1, packets_count::11, payload::bits>>, [packet | packets]) do
-      packet =
-        packet
-        |> Map.put(:type, type)
-        |> Map.put(:length_type_id, 1)
+      packet = handle_operator_data(packet, type, 1)
 
       case decode_operator(:count, packets_count, payload, []) do
         {rest, subpackets} ->
-          {rest, [process_subpackets(packet, subpackets) | packets]}
+          {rest, [handle_operator_subpackets_data(packet, subpackets) | packets]}
 
         subpackets ->
-          [process_subpackets(packet, subpackets) | packets]
+          [handle_operator_subpackets_data(packet, subpackets) | packets]
       end
     end
 
-    # leftover garbage bits
-    defp decode_type(_, [_packet | packets]), do: packets
+    # garbage bits
+    defp decode_type(<<_a::3, _b::1, _c::bits>>, [_packet | packets]), do: packets
 
-    defp process_subpackets(packet, subpackets) do
-      subpackets_bit_size =
-        Enum.reduce(subpackets, 0, fn s, acc -> acc + Map.get(s, :bit_size) end)
+    defp handle_operator_data(packet, type, length_type_id) do
+      packet
+      |> Map.put(:type, type)
+      |> Map.put(:length_type_id, length_type_id)
+      |> Map.put(:operator, Map.get(@operators, type))
+    end
 
+    defp handle_operator_subpackets_data(packet, subpackets) do
       bit_increment =
         case Map.get(packet, :length_type_id) do
           0 -> 19
@@ -91,7 +106,11 @@ defmodule AdventOfCode2021.Sixteen do
 
       packet
       |> Map.put(:subpackets, subpackets)
-      |> increment_bit_size(bit_increment + subpackets_bit_size)
+      |> increment_bit_size(bit_increment + packets_bit_size(subpackets))
+    end
+
+    defp packets_bit_size(subpackets) do
+      Enum.reduce(subpackets, 0, fn s, acc -> acc + Map.get(s, :bit_size) end)
     end
 
     ##
@@ -110,10 +129,10 @@ defmodule AdventOfCode2021.Sixteen do
           remaining_bit_size =
             length - Enum.reduce(subpackets, 0, fn s, acc -> acc + Map.get(s, :bit_size) end)
 
-          decode_operator(:length, remaining_bit_size, rest, subpackets ++ packets)
+          decode_operator(:length, remaining_bit_size, rest, packets ++ subpackets)
 
         subpackets ->
-          subpackets ++ packets
+          packets ++ subpackets
       end
     end
 
@@ -126,10 +145,10 @@ defmodule AdventOfCode2021.Sixteen do
     def decode_operator(:count, count, payload, packets) do
       case Packet.decode(payload, []) do
         {rest, subpackets} ->
-          decode_operator(:count, count - 1, rest, subpackets ++ packets)
+          decode_operator(:count, count - length(subpackets), rest, packets ++ subpackets)
 
         subpackets ->
-          subpackets ++ packets
+          packets ++ subpackets
       end
     end
 
@@ -154,7 +173,7 @@ defmodule AdventOfCode2021.Sixteen do
         })
         |> increment_bit_size(div(bit_size(literal), 4) * 5)
 
-      {rest, [packet | packets]}
+      {rest, Enum.reverse([packet | packets])}
     end
 
     defp increment_bit_size(packet, amount) do
@@ -163,16 +182,63 @@ defmodule AdventOfCode2021.Sixteen do
   end
 
   def one(input) do
-    Packet.new(input)
+    input
+    |> Packet.new()
     |> traverse()
     |> Enum.sum()
   end
 
-  def two(input_file) do
+  def two(input) do
+    input
+    |> Packet.new()
+    |> evaluate()
+  end
+
+  def test_inputs() do
+    [
+      "D2FE28",
+      "38006F45291200",
+      "EE00D40C823060",
+      "8A004A801A8002F478",
+      "620080001611562C8802118E34",
+      "C0015000016115A2E0802F182340",
+      "A0016C880162017C3686B18A3D4780",
+      "C200B40A82",
+      "04005AC33890",
+      "880086C3E88112",
+      "CE00C43D881120",
+      "D8005AC2A8F0",
+      "F600BC2D8F",
+      "9C005AC2F8F0",
+      "9C0141080250320F1802104A08"
+    ]
+    |> Enum.map(fn i ->
+
+      {i,
+       Packet.new(i)
+       |> evaluate()}
+    end)
   end
 
   # entry case
-  def traverse(packet_list), do: traverse(packet_list, [])
+  def evaluate([packet]), do: evaluate(packet)
+
+  # general case: apply two-argument operator
+  def evaluate(%{type: type, operator: operator} = packet) when type > 4 do
+    apply(operator, Enum.map(Map.get(packet, :subpackets), &evaluate/1))
+  end
+
+  # general case: apply operator
+  def evaluate(%{operator: operator} = packet) do
+    apply(operator, [Enum.map(Map.get(packet, :subpackets), &evaluate/1)])
+  end
+
+  # base case: literal packet
+  def evaluate(packet), do: Map.get(packet, :body)
+
+  # entry case
+  def traverse(packet_list) when is_list(packet_list), do: traverse(packet_list, [])
+  def traverse(packet), do: traverse([packet], [])
 
   # base case
   def traverse([], visited), do: visited
@@ -209,4 +275,10 @@ defmodule AdventOfCode2021.Sixteen do
     padding = 8 - rem(bit_size(input), 8)
     <<0::size(padding), input::bitstring>>
   end
+
+  def gt(a, b), do: if(a > b, do: 1, else: 0)
+
+  def lt(a, b), do: if(a < b, do: 1, else: 0)
+
+  def equality(a, b), do: if(a == b, do: 1, else: 0)
 end
