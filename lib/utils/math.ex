fcb685b6495e0303c0961045b79ad09db8f49e3d
JP <jp.bonner@bonner.jp>
2022-10-27 01:07:00 -0400

    day 19: wip matrix multiplication

--- lib/utils/math.ex
+++ lib/utils/math.ex
@@ -10,40 +10,50 @@ defmodule AdventOfCode2021.Utils.Math do
 end
 
 defmodule AdventOfCode2021.Utils.Math.Vector do
+  defstruct coords: []
+  @spec new([integer()]) :: %AdventOfCode2021.Utils.Math.Vector{coords: [integer()]}
   def new(list) do
-    list
+    %__MODULE__{coords: list}
   end
+
+  @spec dim(%AdventOfCode2021.Utils.Math.Vector{}) :: non_neg_integer
+  def dim(vector = %__MODULE__{}), do: length(vector.coords)
 end
 
 defmodule AdventOfCode2021.Utils.Math.Matrix do
   defstruct rows: [], columns: []
 
+  @spec new([integer()]) :: %AdventOfCode2021.Utils.Math.Matrix{columns: [[integer()]], rows: [[integer()]]}
   def new(lists) do
     %__MODULE__{rows: lists, columns: Enum.zip(lists)}
   end
 
-  def pow(%__MODULE__{} = matrix, {:vector, vector}) do
-    vector
-  end
+  # def pow(%__MODULE__{} = matrix, {:vector, vector}) do
+  #   vector
+  # end
 
-  def pow(%__MODULE__{} = matrix, {:matrix, matrix}) do
-    matrix
-  end
+  # def pow(%__MODULE__{} = matrix, {:matrix, matrix}) do
+  #   matrix
+  # end
 
-  def multiply(matrix, {:vector, vector}) do
-    # for i <- Tuple.to_list(vector), do: i
+  # def multiply(a = %__MODULE__{}, b = %AdventOfCode2021.Utils.Math.Vector{}) do
+  #   # for i <- Tuple.to_list(vector), do: i
 
-    # end
-    for {i, j} <- Enum.zip(Tuple.to_list(vector), matrix) do
-      Enum.sum([i, j])
+  #   # end
+  #   for {i, j} <- Enum.zip(Tuple.to_list(vector), matrix) do
+  #     Enum.sum([i, j])
+  #   end
+  #   for i <- matrix,
+  #       {_j, k} <- Enum.zip(vector, i) do
+  #     i * k
+  #   end
+  # end
+
+  def multiply(%__MODULE__{rows: rows_a, columns: _columns_a}, %__MODULE__{rows: _rows_b, columns: columns_b}) do
+    for {row, column} <- Enum.zip(rows_a, columns_b) do
+      for {a, b} <- Enum.zip(row, column) do
+        a * b
+      end
     end
-    for i <- matrix,
-        {_j, k} <- Enum.zip(vector, i) do
-      i * k
-    end
-  end
-
-  def multiply(matrix, {:matrix, matrix}) do
-    matrix
   end
 end
